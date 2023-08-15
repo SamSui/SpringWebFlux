e68a6848ab7bf66db258380b796e0c02fbdda56b
xisui <xisui@cisco.com>
2023-08-15 15:13:28 +0800

    netty starter webflux

--- webflux/src/main/java/com/example/webflux/controllers/HelloController.java
+++ webflux/src/main/java/com/example/webflux/controllers/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.webflux.controllers;
 
+import com.example.webflux.domain.Post;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -13,5 +16,13 @@ public class HelloController {
     public Mono<String> hello() {
         return Mono.just("Hello Spring Webflux");
     }
-    
+
+    @GetMapping("/posts")
+    public Flux<Post> posts() {
+
+        WebClient webClient = WebClient.create();
+        Flux<Post> postFlux = webClient.get().uri("http://jsonplaceholder.typicode.com/posts").retrieve().bodyToFlux(Post.class);
+
+        return postFlux;
+    }
 }
