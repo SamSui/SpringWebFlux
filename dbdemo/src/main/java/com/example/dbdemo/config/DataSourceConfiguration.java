4b2e3a4bdea1ac02435d8544817f35fe5f7add48
suixiangxi <suixiaox@163.com>
2023-12-19 13:59:10 +0800

    3rd Encrypt for db

--- dbdemo/src/main/java/com/example/dbdemo/config/DataSourceConfiguration.java
+++ dbdemo/src/main/java/com/example/dbdemo/config/DataSourceConfiguration.java
@@ -31,13 +31,13 @@ public class DataSourceConfiguration {
             return new JdbcDynamicDataSource(dataSourceProperties);
         }
     }
-    
+
     @Configuration
     @ConditionalOnProperty( "catalina.home" )
     @ConditionalOnExpression( "'${catalina.home}' != '.'" )
     protected static class Tomcat {
         private static final Logger logger = LoggerFactory.getLogger(EmbeddedTomcat.class);
-      
+
         @Bean(name = "dataSource")
         DataSource dataSource() {
             logger.info("Found tomcat, so JNDI data sources are being configured.");
@@ -72,4 +72,51 @@ public class DataSourceConfiguration {
         static class OnProperty {
         }
     }
+//    @Bean("tradePlatformDataSource")
+//    public DataSource dataSource(
+//            @Qualifier("druidDataSource") DataSource ds) throws SQLException {
+//        return EncryptDataSourceFactory.createDataSource(ds, getEncryptRuleConfiguration(), new Properties());
+//    }
+
+//    private EncryptRuleConfiguration getEncryptRuleConfiguration() {
+//        Properties props = new Properties();
+//
+//        //自带aes算法需要
+//        props.setProperty("aes.key.value", aeskey);
+//        EncryptorRuleConfiguration encryptorConfig = new EncryptorRuleConfiguration("AES", props);
+//
+//        //自定义算法
+//        //props.setProperty("qb.finance.aes.key.value", aeskey);
+//        //EncryptorRuleConfiguration encryptorConfig = new EncryptorRuleConfiguration("QB-FINANCE-AES", props);
+//
+//        EncryptRuleConfiguration encryptRuleConfig = new EncryptRuleConfiguration();
+//        encryptRuleConfig.getEncryptors().put("aes", encryptorConfig);
+//
+//        //START: card_info 表的脱敏配置
+//        {
+//            EncryptColumnRuleConfiguration columnConfig1 = new EncryptColumnRuleConfiguration("", "name", "", "aes");
+//            EncryptColumnRuleConfiguration columnConfig2 = new EncryptColumnRuleConfiguration("", "id_no", "", "aes");
+//            EncryptColumnRuleConfiguration columnConfig3 = new EncryptColumnRuleConfiguration("", "finshell_card_no", "", "aes");
+//            Map<String, EncryptColumnRuleConfiguration> columnConfigMaps = new HashMap<>();
+//            columnConfigMaps.put("name", columnConfig1);
+//            columnConfigMaps.put("id_no", columnConfig2);
+//            columnConfigMaps.put("finshell_card_no", columnConfig3);
+//            EncryptTableRuleConfiguration tableConfig = new EncryptTableRuleConfiguration(columnConfigMaps);
+//            encryptRuleConfig.getTables().put("card_info", tableConfig);
+//        }
+//        //END: card_info 表的脱敏配置
+//
+//        //START: pay_order 表的脱敏配置
+//        {
+//            EncryptColumnRuleConfiguration columnConfig1 = new EncryptColumnRuleConfiguration("", "card_no", "", "aes");
+//            Map<String, EncryptColumnRuleConfiguration> columnConfigMaps = new HashMap<>();
+//            columnConfigMaps.put("card_no", columnConfig1);
+//            EncryptTableRuleConfiguration tableConfig = new EncryptTableRuleConfiguration(columnConfigMaps);
+//            encryptRuleConfig.getTables().put("pay_order", tableConfig);
+//        }
+//
+//        log.info("脱敏配置构建完成:{} ", encryptRuleConfig);
+//        return encryptRuleConfig;
+//
+//    }
 }
